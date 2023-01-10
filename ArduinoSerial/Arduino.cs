5540f247854aeae10cc6c5b3dfbc7d8bf91df030
Narzaru <yaroslahek@gmail.com>
2023-01-11 01:33:02 +0700

    InitCommit

---
+++ ArduinoSerial/Arduino.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Timers;
+using System.IO.Ports;
+
+namespace ArduinoSerial;
+
+public class Arduino
+{
+    public Arduino()
+    {
+        PortName = string.Empty;
+    }
+
+    public bool AutodetectArduinoPort(int boundRate, TimeSpan timeout)
+    {
+        var ports = SerialPort.GetPortNames();
+        foreach (var portName in ports)
+        {
+            try
+            {
+                SerialPort port = new SerialPort(portName, boundRate);
+                if (!port.IsOpen)
+                {
+                    port.Open();
+                    port.ReadTimeout = (int)timeout.TotalMilliseconds;
+                    port.Write("detect");
+                    var answer = port.ReadLine();
+                    if (answer.Contains("detected"))
+                    {
+                        PortName = portName;
+                        port.Close();
+                        return true;
+                    }
+                    port.Close();
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        return false;
+    }
+
+    public bool PortDetected() => !PortName.Equals(String.Empty);
+
+    public string PortName
+    {
+        get;
+        private set;
+    }
+}
---
+++ ArduinoSerial/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ArduinoSerial
+{
+    internal static class Program
+    {
+        public static int Main(string[] args)
+        {
+            // try find arduino with my sketch
+            var serial = new Arduino();
+            if (serial.AutodetectArduinoPort(9600, TimeSpan.FromSeconds(2)))
+            {
+                Console.WriteLine($"detected on {serial.PortName}");
+            } else
+            {
+                Console.WriteLine("doesnt detected");
+                return -1;
+            }
+
+            // open arduino port
+            var port = new SerialPort(serial.PortName, 9600);
+            port.ReadTimeout = (int)TimeSpan.FromSeconds(5).TotalMilliseconds;
+            port.Open();
+
+            port.DataReceived += (sender, e) =>
+            {
+                Console.WriteLine(port.ReadLine());
+            };
+
+            // do commands
+            string command;
+            do
+            {
+                command = Console.ReadLine()!;
+                port.Write(command);
+            } while (command != "exit");
+            
+            port.Close();
+            return 0;
+        }
+    }
+}
