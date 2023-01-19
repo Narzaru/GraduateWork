047a6110c8fdfd646d71a4142e1d8c5a18e71563
Narzaru <yaroslahek@gmail.com>
2023-01-19 23:20:04 +0700

    Implement data base migration

---
+++ Data/Migrations/20230119155936_Migrations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PointsSet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CreationTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Label = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PointsSet", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Points",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PositionX = table.Column<float>(type: "REAL", nullable: false),
+                    PositionY = table.Column<float>(type: "REAL", nullable: false),
+                    PointsSetId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Points", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Points_PointsSet_PointsSetId",
+                        column: x => x.PointsSetId,
+                        principalTable: "PointsSet",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Points_PointsSetId",
+                table: "Points",
+                column: "PointsSetId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Points");
+
+            migrationBuilder.DropTable(
+                name: "PointsSet");
+        }
+    }
+}
