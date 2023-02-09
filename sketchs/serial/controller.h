4295445c3a3003d6a2d83a9edb36b757b9a435d2
Narzaru <yaroslahek@gmail.com>
2023-02-09 16:37:05 +0700

    Attempt to make a non-blocking serial port

--- sketchs/serial/controller.h
+++ sketchs/serial/controller.h
@@ -7,33 +7,87 @@
 #include "protocol_defines.h"
 #include "serial_protocol.h"
 
+enum class State {
+  kNone,
+  kSetPoints,
+  kMoveToPoints,
+  kMoveToPointsInProgress
+};
+
 class Controller {
 public:
-  Controller(protocol::SerialDriver &driver) : driver_(driver) {}
+  Controller(protocol::SerialDriver &driver) : driver_(driver), state_(State::kNone) {}
 
-  // По задумке это место должно являться просто стейт машиной.
-  // Некая точка входа в обработку данных.
+  // Некая точка входа в обработку данных, принятие решения, что надо делать.
+  // Конкретная реализация дейсвтия управлению мотором и.д не должна производиться тут.
+  // Тут только общение со внешним устройством.
   void ProcessData(protocol::Data *data) {
-      if (data->Command == PROTOCOL_COMMAND_ECHO) {
-        EchoHandler(data);
-      } else if (data->Command == PROTOCOL_COMMAND_SET_POINTS) {
-        SetPointsHandler(data);
-      } else if (data->Command == PROTOCOL_COMMAND_MESSAGE_ERROR) {
-        MessageErrorHandler(data);
-      }
+    command_data_ = *data;
+    if (data->Command == PROTOCOL_COMMAND_ECHO) {
+      EchoHandler(data);
+    } else if (data->Command == PROTOCOL_COMMAND_SET_POINTS) {
+      SetPointsHandler(data);
+    } else if (data->Command == PROTOCOL_COMMAND_MESSAGE_ERROR) {
+      MessageErrorHandler(data);
+    } else if (data->Command == PROTOCOL_COMMAND_MOVE) {
+      MoveToPointsHandler(data);
+    }
+  }
+
+  void SetPointsCompleted(bool success, protocol::Data *data) {
+    memset(&data->Payload, 0, sizeof(data->Payload));
+    if (success) {
+      memcpy(&data->Payload.Message, "POINTS SET", 10);
+      data->Command = PROTOCOL_COMMAND_SET_POINTS_SUCCESS;
+    } else {
+      memcpy(&data->Payload.Message, "POINTS ERROR", 12);
+      data->Command = PROTOCOL_COMMAND_SET_POINTS_FAILD;
+    }
+    driver_.WriteData(*data);
+    state_ = State::kNone;
+  }
+
+  void MoveToPointsCompleted(bool success, protocol::Data *data) {
+    memset(&data->Payload, 0, sizeof(data->Payload));
+    if (success) {
+      memcpy(&data->Payload.Message, "MOVED", 5);
+      data->Command = PROTOCOL_COMMAND_MOVE_COMPLETED;
+    } else {
+      memcpy(&data->Payload.Message, "MOVE BLOCKED", 12);
+      data->Command = PROTOCOL_COMMAND_MOVE_FAILD;
+    }
+    driver_.WriteData(*data);
+    state_ = State::kNone;
+  }
+
+  void MoveToPointsInProgress() {
+    state_ = State::kMoveToPointsInProgress;
+  }
+
+  bool IsHaveCommand() const {
+    return !(state_ == State::kNone || state_ == State::kMoveToPointsInProgress);
+  }
+   
+  const protocol::Data& get_data() const {
+    return command_data_;
+  }
+
+  State get_command() const {
+    return state_;
   }
 
 private:
-  // Методы для каждого случая
+  // Методы для каждой команды
   void EchoHandler(protocol::Data *data) {
     driver_.WriteData(*data);
   }
 
   void SetPointsHandler(protocol::Data *data) {
-    memset(&data->Payload, 0, sizeof(data->Payload));
-    memcpy(&data->Payload.Message, "POINTS SET", 10);
-    data->Command = PROTOCOL_COMMAND_SET_POINTS_SUCCESS;
-    driver_.WriteData(*data);
+    state_ = State::kSetPoints;
+  }
+
+  void MoveToPointsHandler(protocol::Data *data) {
+    state_ = State::kMoveToPoints;
   }
 
   void MessageErrorHandler(protocol::Data *data) {
@@ -42,6 +96,8 @@ private:
   }
 
   protocol::SerialDriver &driver_;
+  protocol::Data command_data_;
+  State state_;
 };
 
 #endif  // SERIAL_CONTROLLER_H_
--- sketchs/serial/serial.ino
+++ sketchs/serial/serial.ino
@@ -5,7 +5,6 @@ protocol::SerialDriver serial_driver(Serial);
 Controller controller(serial_driver);
 bool is_data_proccessed;
 protocol::Data data;
-protocol::PointsSet points;
 
 void serialEvent() {
   data = serial_driver.ReadData();
@@ -22,11 +21,38 @@ void serialEvent() {
 void setup() {
   Serial.begin(115200);
   while (!Serial) {}
+  Serial.write("Inited!");
 }
 
+//// макаронный код, пока нормального решения не придумано...
+// флаг, чтобы раз за цикл заходить и по чуть-чуть двигать моторы
+bool is_moving;
+int i = 0;
 void loop() {
+  // если есть необработанная комманда
   if (!is_data_proccessed) {
     controller.ProcessData(&data);
+    is_data_proccessed = true;
   }
-  is_data_proccessed = true;
+  // если есть необработнная комманда для мк
+  if (controller.IsHaveCommand()) {
+    if (controller.get_command() == State::kSetPoints) {
+      auto data = controller.get_data();
+      // validate points
+      // if points valid - true, else - false
+      controller.SetPointsCompleted(false, &data);
+    } else if (controller.get_command() == State::kMoveToPoints) {
+      is_moving = true;
+      controller.MoveToPointsInProgress();
+    }
+  }
+  if (is_moving) {
+    delay(1000);
+    ++i;
+    if (i >= 5) {
+      i = 0;
+      is_moving = false;
+      controller.MoveToPointsCompleted(true, &data);
+    }
+  }
 }
